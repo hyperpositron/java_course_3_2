2c56bcbfa00d31b1420546f581022e7e18bf5e7d
Александр <hyperpositron@gmail.com>
2023-06-23 11:38:10 +0200

    first commit

---
+++ src/test/java/pro/sky/howework_2/Howework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.howework_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Howework2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
